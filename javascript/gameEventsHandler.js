dc28bfdbcd1a0cd7f075bee6c7d1606ba7cb7cf8
Uriel García Rivas <uriel_fable@hotmail.com>
2020-03-10 19:45:04 -0600

    Javascript works! Plus board drawn.

--- javascript/gameEventsHandler.js
+++ javascript/gameEventsHandler.js
@@ -1,2 +1,21 @@
 var canvasBoard = document.getElementById("board");
+var contextBoard = canvasBoard.getContext("2d");
+
+function setUp(){
+  drawBoard();
+}
+
+function drawBoard(){
+  let k = 0;
+  for(let i = 0; i < 8; i++){
+    for(let j = 0; j < 8; j++){
+      if(k % 2 != 0) contextBoard.fillStyle = "#67ab23"; /* green squares */
+      else contextBoard.fillStyle = "#f0f0da"; /* green squares */
+      contextBoard.fillRect(100*j, 100*i, 100, 100);
+      k++;
+    }
+    k++;
+  }
+}
+
 canvasBoard.addEventListener("click", function(){}, false);
