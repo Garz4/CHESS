1edae5f8f201895844171a546a3fdfb52ade8461
Uriel García Rivas <uriel_fable@hotmail.com>
2020-03-12 12:24:24 -0600

    Moved the code from "gameEventsHander" to "gameMain".

--- javascript/gameEventsHandler.js
+++ javascript/gameEventsHandler.js
@@ -1,31 +1,13 @@
 /*
  * Coded by: Garz4 (Uriel García Rivas).
  */
-var canvasBoard = document.getElementById("board");
-var contextBoard = canvasBoard.getContext("2d");
-canvasBoard.addEventListener("click", function(){}, false);
-
-function setUp(){
-  drawBoardForWhite();
+function getMousePos(evt) {
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
 }
 
-function drawBoardForWhite(){drawBoard("87654321", "abcdefgh", 0);}
-function drawBoardForBlack(){drawBoard("12345678", "hgfedcba", 1);}
-
-function drawBoard(num, letter, k){
-  for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
-      if(k % 2 != 0) contextBoard.fillStyle = "#67ab23"; /* green squares */
-      else contextBoard.fillStyle = "#f0f0da"; /* green squares */
-      contextBoard.fillRect(100*j, 100*i, 100, 100);
-      k++;
-    }
-    k++;
-  }
-  contextBoard.font = "25px Arial";
-  contextBoard.fillStyle = "#000000";
-  for(let i = 0; i < 8; i++){
-    contextBoard.fillText(num[i], 5, 100*i + 25); /* rows (numbers) */
-    contextBoard.fillText(letter[i], 100*i + 80, 795); /* cols (letters) */
-  }
+function onclickHandler(e){
+  console.log(getMousePos(e));
 }
--- javascript/gameMain.js
+++ javascript/gameMain.js
@@ -1,3 +1,54 @@
 /*
  * Coded by: Garz4 (Uriel García Rivas).
  */
+var canvasBoard = document.getElementById("board");
+var rect = canvasBoard.getBoundingClientRect();
+var contextBoard = canvasBoard.getContext("2d");
+var turno = 0;
+
+function drawBoard(num, letter, k){
+ for(let i = 0; i < 8; i++){
+   for(let j = 0; j < 8; j++){
+     if(k % 2 != 0) contextBoard.fillStyle = "#67ab23"; /* green squares */
+     else contextBoard.fillStyle = "#f0f0da"; /* white squares */
+     contextBoard.fillRect(100*j, 100*i, 100, 100);
+     k++;
+   }
+   k++;
+ }
+ contextBoard.font = "25px Arial";
+ contextBoard.fillStyle = "#000000";
+ for(let i = 0; i < 8; i++){
+   contextBoard.fillText(num[i], 5, 100*i + 25); /* rows (numbers) */
+   contextBoard.fillText(letter[i], 100*i + 80, 795); /* cols (letters) */
+ }
+}
+
+function drawBoardForWhite(){drawBoard("87654321", "abcdefgh", 0);}
+function drawBoardForBlack(){drawBoard("12345678", "hgfedcba", 1);}
+
+function setUp(){
+ canvasBoard.setAttribute("style", "border:5px solid #67ab23;");
+ drawBoardForWhite();
+}
+
+function startOffline(){
+
+}
+
+function startOnline(){
+
+}
+
+class Piece {
+  constructor(x, y, id){
+    this.movesAllowed = [];
+    this.x = x;
+    this.y = y;
+    this.img = new Image();
+    img.onload = function() {
+        contextBoard.drawImage(this.img, this.x, this.y, this.x+95, this.y+95);
+    }
+    img.src = "pieces/Chess_qlt45.svg";
+  }
+}
